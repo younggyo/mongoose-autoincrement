fe944389dfc9c6af0b9da5c53a8e6601d79ebed1
Holly <hl4god@gmail.com>
2016-10-12 13:49:27 +0800

    change update options for mongodb drive 2.2

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 'use strict';
-
-var autoIncrement = function(schema, options) {
+var autoIncrement = function (schema, options) {
   schema.add({
-    _id: { type: Number, unique: true }
+    _id: { type: Number, index: true, unique: true },
   });
-
-  schema.pre('save', function(next) {
+  schema.pre('save', function (next) {
     var doc = this;
     if (doc.isNew) {
-      doc._id = getNextSeq(doc.db.db, doc.collection.name, function(err, seq) {
+      getNextSeq(doc.db.db, doc.collection.name, function (err, seq) {
         if (err) next(err);
         doc._id = seq;
         next();
@@ -17,12 +15,11 @@ var autoIncrement = function(schema, options) {
   });
 };
 
-var getNextSeq = function(db, name, callback) {
-  db.collection('counters').findAndModify(
-    { _id: name },
-    { _id: -1 },
+var getNextSeq = function (db, name, callback) {
+  db.collection('counters').findOneAndUpdate(
+    { model: name },
     { $inc: { seq: 1 } },
-    { new: true, upsert: true },
+    { returnOriginal: false, upsert: true },
     function (err, ret) {
       if (err) callback(err);
       callback(null, ret.value.seq);
