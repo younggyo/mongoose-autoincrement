6f00f50ebc3093bb339af69f254345400ef3f3e3
Holly <hl4god@gmail.com>
2016-11-04 10:34:43 +0800

    update

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 var autoIncrement = function (schema, options) {
   schema.add({
     _id: { type: Number, index: true, unique: true },
+    updateAt: { type: Date, default: Date.now },
   });
   schema.pre('save', function (next) {
     var doc = this;
+    doc.updateAt = Date.now();
     if (doc.db && doc.isNew && typeof doc._id === 'undefined') {
       return getNextSeq(doc.db.db, doc.collection.name, function (err, seq) {
         if (err) next(err);
